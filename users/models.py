e24e2a46b9e5a0232586dab2c36bf2eaf0ef0427
Rohit Raj Anand <rohitraazanand567@gmail.com>
2018-09-20 14:35:32 +0530

    Backend - Rohit users app - version - 1.1 static files. default user profile pic. Icon. Nav-Image. Minor Bug fixes.

--- users/models.py
+++ users/models.py
@@ -2,17 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import datetime, os
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profilepic = models.ImageField(upload_to = 'profile/', blank=True)
-    DOB = models.DateField()
+    DOB = models.DateField(('Date'), default= datetime.date.today)
     college = models.CharField(max_length = 50)
     # degree = models.CharField(max_length =30)
     bio = models.TextField()
 
     def __str__(self):
         return self.user.username
+
+    @property
+    def image_url(self):
+        if self.profilepic:
+            return self.profilepic.url
+        else:
+            return r"/static/user.png"
 
 
 @receiver(post_save, sender=User)
