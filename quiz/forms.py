6f07444c2975071fa35ce503cf1eeaa0a1a5736b
Rohit Raj Anand <rohitraazanand567@gmail.com>
2018-09-27 16:00:03 +0530

    Frontend - Rohit Instructions to add quiz were added to createquiz.html

--- quiz/forms.py
+++ quiz/forms.py
@@ -7,13 +7,13 @@ class QuizForm(forms.ModelForm):
         fields = ('Quiz_id', 'name', 'Test_Password', 'about', 'instructions', 'csv_file', 'positive', 'negative', 'duration')
 
         widgets = {
-            'about': forms.Textarea(attrs={'class':'form-control col-8'}),
-            'instructions': forms.Textarea(attrs={'class':'form-control col-8'}),
-            'name' : forms.TextInput(attrs={'class':'form-control col-6'}),
-            'Test_Password' :forms.TextInput(attrs={'type' : 'password','class':'form-control col-6'}),
-            'positive' : forms.NumberInput(attrs = {'class':'form-control col-3'}),
-            'negative' : forms.NumberInput(attrs = {'class':'form-control col-3'}),
-            'duration' : forms.TimeInput(attrs = {'class':'form-control col-3'}),
-            'Quiz_id': forms.TextInput(attrs={'class': 'form-control col-6'}),
+            'about': forms.Textarea(attrs={'class':'form-control col-11', 'cols': '10'}),
+            'instructions': forms.Textarea(attrs={'class':'form-control col-11','cols': '10'}),
+            'name' : forms.TextInput(attrs={'class':'form-control col-9'}),
+            'Test_Password' :forms.TextInput(attrs={'type' : 'password','class':'form-control col-9'}),
+            'positive' : forms.NumberInput(attrs = {'class':'form-control col-4'}),
+            'negative' : forms.NumberInput(attrs = {'class':'form-control col-4'}),
+            'duration' : forms.TimeInput(attrs = {'class':'form-control col-4'}),
+            'Quiz_id': forms.TextInput(attrs={'class': 'form-control col-9'}),
             'csv_file': forms.FileInput(attrs={'class': 'form-control col-5 btn btn-primary btn-sm'})
             }
